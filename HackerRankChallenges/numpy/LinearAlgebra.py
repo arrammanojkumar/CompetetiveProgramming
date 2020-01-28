cb8843add3b94969de79597157f0f500eac29fb0
Manoj Kumar <maarram@innominds.com>
2020-01-28 16:38:18 +0530

    solved small problem

--- HackerRankChallenges/numpy/LinearAlgebra.py
+++ HackerRankChallenges/numpy/LinearAlgebra.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 if __name__ == "__main__":
-    size = int(raw_input())
+    size = int(input())
     matrix = list(
-        list(float(_1) for _1 in raw_input().split(' ')) for _ in range(size)
+        list(float(_1) for _1 in input().split(' ')) for _ in range(size)
     )
-    print round(np.linalg.det(matrix), 2)
+    print(round(np.linalg.det(matrix), 2))
