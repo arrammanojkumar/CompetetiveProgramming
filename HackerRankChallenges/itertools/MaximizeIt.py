b10e98aad77ae27183d257c6410dce1b2f78173c
Manoj Kumar <manoo2mail@gmail.com>
2020-02-12 12:26:57 +0530

    sovled itertools

---
+++ HackerRankChallenges/itertools/MaximizeIt.py
@@ -0,0 +1,9 @@
+__author__ = "Manoj Kumar Arram"
+
+from itertools import product
+
+if __name__ == "__main__":
+    K, M = map(int, input().split())
+    N = (list(map(int, input().split()))[1:] for _ in range(K))
+    results = map(lambda x: sum(i ** 2 for i in x) % M, product(*N))
+    print(max(results))
