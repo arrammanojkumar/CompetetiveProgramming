0456f19fb6300a950365099aa37bf64bf9b0c042
Manoj Kumar <manoo2mail@gmail.com>
2020-03-04 00:53:01 +0530

    solved classes

---
+++ HackerRankChallenges/classes/ComplexNumbers.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+__author__ = "Manoj Kumar Arram"
+
+import math
+
+
+class Complex(object):
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+
+    def __add__(self, other):
+        return Complex(self.real + other.real, self.imaginary + other.imaginary)
+
+    def __sub__(self, other):
+        return Complex(self.real - other.real, self.imaginary - other.imaginary)
+
+    def __mul__(self, other):
+        return Complex(self.real * other.real - self.imaginary * other.imaginary,
+                       self.real * other.imaginary + self.imaginary * other.real)
+
+    def __truediv__(self, other):
+        try:
+            return self.__mul__(Complex(other.real, -1 * other.imaginary)).__mul__(
+                Complex(1.0 / other.mod().real ** 2, 0))
+        except ZeroDivisionError as e:
+            print(e)
+
+    def mod(self):
+        return Complex(pow(self.real ** 2 + self.imaginary ** 2, 0.5), 0)
+
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % self.real
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % self.imaginary
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+
+if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x + y, x - y, x * y, x / y, x.mod(), y.mod()]), sep='\n')
