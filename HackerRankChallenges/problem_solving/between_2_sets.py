3dc24c6f5ac5e9323a0be9550562d70695d08bae
Manoj Kumar <manoo2mail@gmail.com>
2023-12-14 23:39:59 +0530

    solved two problems

---
+++ HackerRankChallenges/problem_solving/between_2_sets.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    # Write your code here
+    max_a = max(a)
+    min_b = min(b) + 1
+    
+    selected = 0
+    for choice in range(max_a, min_b):
+        if all(choice % a_el == 0 for a_el in a) and all(b_el % choice == 0 for b_el in b):
+            selected += 1
+    return selected
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
