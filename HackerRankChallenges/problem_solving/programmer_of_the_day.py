e02e6ca9b744f6e15b25c961f938d40cfaa0f2d4
Manoj Kumar <manoo2mail@gmail.com>
2024-07-14 22:32:08 +0530

    solved one more problem

--- HackerRankChallenges/problem_solving/programmer_of_the_day.py
+++ HackerRankChallenges/problem_solving/programmer_of_the_day.py
@@ -18,7 +18,7 @@ import os
 
 def dayOfProgrammer(year):
     # Write your code here
-    if (year == 1918):
+    if year == 1918:
         return '26.09.1918'
     elif ((year <= 1917) & (year % 4 == 0)) or (
             (year > 1918) & (year % 400 == 0 or ((year % 4 == 0) & (year % 100 != 0)))):
