87a46e5af26afa439b8c56a9c3c6ff704e5e7deb
Manoj Kumar <manoo2mail@gmail.com>
2024-09-10 00:25:18 +0530

    longest palindrome substring

--- leetcode/longest_palindrome_substring.py
+++ leetcode/longest_palindrome_substring.py
@@ -1,3 +1,33 @@
 #!/bin/python3
 
 __author__ = "Manoj Kumar Arram"
+"""
+Given a string s, return the longest palindromic substring in s.
+
+https://leetcode.com/problems/longest-palindromic-substring/description/
+"""
+
+
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+def is_palindrome(string):
+    mid = len(string)/2
+    last = len(string)-1
+    start = 0
+    while last >= mid or start <= mid:
+        if not string[last] == string[start]:
+            return False
+        last -= 1
+        start += 1
+    return True
+
+
+def substrings(s):
+    for length in range(len(s), 0, -1):
+        for start in range(len(s) - length + 1):
+            string = s[start: start + length]
+            if is_palindrome(string):
+                return string
+            
+
+print(substrings("abb"))
